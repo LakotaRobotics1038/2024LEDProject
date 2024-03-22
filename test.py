42fe5d688bbafcc245967fb1305c8e216b3b1a9b
Murat65536 <bmuratb502@gmail.com>
2024-03-22 16:42:24 -0400

    Update set_mode

--- test.py
+++ test.py
@@ -55,89 +55,57 @@ class NeopixelController:
 
     async def racing(self, strip: int, baseColor: "tuple[int, int, int]", racingColor: "tuple[int, int, int]", length: int, delay: float) -> None:
         while True:
-            for led in range(self.led_starting_positions[strip - 1], len(self.leds[self.led_strip[strip - 1]])):
+            for led in range(self.led_starting_positions[strip - 1], self.led_count[strip - 1]):
                 self.leds[self.led_strip[strip - 1]][led] = racingColor
                 if led - length >= self.led_starting_positions[strip - 1]:
                     self.leds[self.led_strip[strip - 1]][led - length] = baseColor
                 if led - length < self.led_starting_positions[strip - 1]:
-                    self.leds[self.led_strip[strip - 1]][len(self.leds[self.led_strip[strip - 1]]) - self.led_starting_positions[strip - 1] + led - length] = baseColor
+                    self.leds[self.led_strip[strip - 1]][self.led_count[strip - 1] - self.led_starting_positions[strip - 1] + led - length] = baseColor
                 self.leds[self.led_strip[strip - 1]].write()
                 await uasyncio.sleep(delay)
 
-async def set_mode():
+async def set_mode() -> None:
     character = "D"
     select_poll = uselect.poll()
     select_poll.register(usys.stdin, 1)
-    tasks = {"1":None, "2":None, "3":None, "4":None, "5":None, "6":None}
+    tasks = [["", None], ["", None], ["", None], ["", None], ["", None], ["", None]]
+    mode = {
+        "D":[True, True, True, True, True, True],
+        "E":[True, True, True, True, True, True],
+        "N":[False, True, False, True, True, True],
+        "Q":[True, True, False, False, False, False],
+        "G":[True, True, True, True, True, True]
+    }
     
     while True:
         if select_poll.poll(0):
             recieved_input = usys.stdin.read(1)
             if recieved_input != "\n":
                 character = recieved_input
-        if character == "D":
-            for name, task in tasks.items():
-                if name == "D2":
-                    continue
-                elif name.find("2") != -1:
-                    try:
-                        task.cancel()
-                    except:
-                        pass
-                    del tasks[name]
-                    tasks.update({"D2":uasyncio.create_task(np.team_colors(strip=2, start_color=(0, 0, 200), end_color=(200, 0, 200), mix=128, delay=0.01))})
-        elif character == "E":
-            for name, task in tasks.items():
-                if name == "E1":
-                    continue
-                elif name.find("1") != -1:
-                    try:
-                        task.cancel()
-                    except:
-                        pass
-                    del tasks[name]
-                    tasks.update({"E1":uasyncio.create_task(np.rainbow(strip=1, mix=128, delay=0.01))})
-        elif character == "N":
-            for name, task in tasks.items():
-                if name == "N2":
-                    continue
-                elif name.find("2") != -1:
-                    try:
-                        task.cancel()
-                    except:
-                        pass
-                    del tasks[name]
-                    tasks.update({"N2":uasyncio.create_task(np.static_color(strip=2, color=(255, 40, 0), delay=1))})
-        elif character == "Q":
-            for name, task in tasks.items():
-                if name == "Q3":
-                    continue
-                elif name.find("3") != -1:
-                    try:
-                        task.cancel()
-                    except:
-                        pass
-                    del tasks[name]
-                    tasks.update({"Q3":uasyncio.create_task(np.racing(strip=3, baseColor=(0, 0, 200), racingColor=(200, 0, 200), length=3, delay=0.1))})
-        elif character == "G":
-            for name, task in tasks.items():
-                if name == "G1":
-                    continue
-                elif name.find("1") != -1:
-                    try:
-                        task.cancel()
-                    except:
-                        pass
-                    del tasks[name]
-                    tasks.update({"G1":uasyncio.create_task(np.static_color(strip=1, color=(0, 255, 0), delay=2))})
-        await uasyncio.sleep(0.1)
+        for count, task in enumerate(tasks):
+            if task[0] != character and mode[character][count]:
+                try:
+                    task[1].cancel()
+                except:
+                    pass
+                if character == "D":
+                    tasks[count] = [character, uasyncio.create_task(np.team_colors(strip=count + 1, start_color=(0, 0, 200), end_color=(200, 0, 200), mix=128, delay=0.01))]
+                elif character == "E":
+                    tasks[count] = [character, uasyncio.create_task(np.rainbow(strip=count + 1, mix=128, delay=0.01))]
+                elif character == "N":
+                    tasks[count] = [character, uasyncio.create_task(np.static_color(strip=count + 1, color=(255, 40, 0), delay=1))]
+                elif character == "Q":
+                    tasks[count] = [character, uasyncio.create_task(np.racing(strip=count + 1, baseColor=(0, 0, 200), racingColor=(200, 0, 200), length=3, delay=0.1))]
+                elif character == "G":
+                    tasks[count] = [character, uasyncio.create_task(np.static_color(strip=count + 1, color=(0, 255, 0), delay=2))]
+        await uasyncio.sleep(0.01)
 
 np = NeopixelController(pin_numbers=[2, 3, 4, 5], pin_counts=[26, 44, 12, 26], leds=[
-                                                                                        [{"start":1, "count":10}, {"start":11, "count":20}, {"start":21, "count":26}],
-                                                                                        [{"start":1, "count":44}],
-                                                                                        [{"start":1, "count":12}],
-                                                                                        [{"start":1, "count":26}]
-                                                                                    ])
+    [{"start":1, "count":10}, {"start":11, "count":20}, {"start":21, "count":26}],
+    [{"start":1, "count":44}],
+    [{"start":1, "count":12}],
+    [{"start":1, "count":26}]
+])
 
 
 try:
