b1ac75b5fd2f6ea064b029b47e7fe07f1181582e
Murat65536 <bmuratb502@gmail.com>
2024-11-23 11:34:24 -0500

    Revamp chasing LEDs with color fade and sine waves.

--- README.md
+++ README.md
@@ -10,11 +10,11 @@ The LED project for FRC team 1038
 
 1. Clone the project
 2. Install MicroPico extension from the workspace recommended
-3. `CTRL+SHFT+P` and run `MicroPico: Configure project`
+3. `CTRL+SHIFT+P` and run `MicroPico: Configure project`
 
 ## Troubleshooting
 
-- If this is not the first time that program has been flashed, empty the flash memory [here](https://www.raspberrypi.com/documentation/microcontrollers/pico-series.html#resetting-flash-memory/https://datasheets.raspberrypi.com/soft/flash_nuke.uf2) and re-flash the program [here](https://micropython.org/download/RPI_PICO/).
+- If this is not the first time that program has been flashed, empty the flash memory [here](https://datasheets.raspberrypi.com/soft/flash_nuke.uf2) and re-flash the program [here](https://micropython.org/download/RPI_PICO/).
 - If the animation does not start, disconnect the Pico from all sources of power and plug it back in. This includes the USB port and GPIO pins.
 
 ## Modes
--- main.py
+++ main.py
@@ -3,48 +3,67 @@ from umachine import Pin, UART, soft_reset
 from neopixel import NeoPixel
 from usys import stdin, print_exception
 from uselect import poll, POLLIN
-from gc import enable
 from micropython import const
 from rp2 import bootsel_button
+from math import sin
 
-enable()
 
 class NeopixelController:
-
-    def __init__(self, pin_numbers: "list[int]", pin_counts: "list[int]", leds: "list[list[dict[str, int]]]") -> None:
+    def __init__(
+        self,
+        pin_numbers: "tuple[int, ...]",
+        pin_counts: "tuple[int, ...]",
+        leds: "tuple[tuple[dict[str, int], ...], ...]",
+    ) -> None:
         if len(pin_numbers) != len(pin_counts):
             raise ValueError(f"Pin Numbers and Pin Counts must be the same length. Current lengths are {len(pin_numbers)} pin numbers and {len(pin_counts)} leds.")
-        self.leds = []
-        self.led_starting_positions = []
-        self.led_count = []
-        self.led_strip = []
+        self.leds: list[NeoPixel] = []
+        self.start: list[int] = []
+        self.end: list[int] = []
+        self.led_strip: list[int] = []
+        self.led_count: list[int] = []
         for pin, count in zip(pin_numbers, pin_counts):
             self.leds.append(NeoPixel(Pin(pin), count))
         for count, strip in enumerate(leds):
             for portion in strip:
                 self.led_strip.append(count)
-                self.led_starting_positions.append(portion["start"] - 1)
-                self.led_count.append(portion["count"])
+                self.start.append(portion["start"] - 1)
+                self.end.append(portion["end"])
+                self.led_count.append(portion["end"] - portion["start"] - 1)
 
-    async def color_fade(self, strip: int, colors: "list[tuple[int, int, int]]", mix: int, step_delay: float, delay: float) -> None:
+    async def color_fade(
+        self,
+        strip: int,
+        colors: "list[tuple[int, int, int]]",
+        mix: int,
+        step_delay: float,
+        delay: float,
+    ) -> None:
         while True:
             for count in range(len(colors)):
                 for fade_step in range(mix + 1):
-                    intermediate_color = tuple(int((1 - fade_step / mix) * rgb_1 + fade_step / mix * rgb_2) for rgb_1, rgb_2 in zip(colors[count], colors[count + 1] if len(colors) > count + 1 else colors[0]))
-                    for led in range(self.led_count[strip] - self.led_starting_positions[strip]):
-                        self.leds[self.led_strip[strip]][self.led_starting_positions[strip] + led] = intermediate_color
+                    intermediate_color = tuple(int((1 - fade_step / mix) * rgb_1 + fade_step / mix * rgb_2) for rgb_1, rgb_2 in zip(colors[count], colors[(count + 1) % len(colors)]))
+                    for led in range(self.led_count[strip]):
+                        self.leds[self.led_strip[strip]][self.start[strip] + led] = intermediate_color
                     self.leds[self.led_strip[strip]].write()
                     await sleep(step_delay)
             await sleep(delay)
 
-    async def static_color(self, strip: int, color: "tuple[int, int, int]", delay: int, kill: bool, kill_mode: str) -> None:
+    async def static_color(
+        self,
+        strip: int,
+        color: "tuple[int, int, int]",
+        delay: int,
+        kill: bool,
+        kill_mode: str,
+    ) -> None:
         global tasks
         global MODES
         global FUNCTIONS
         global character
 
-        for led in range(self.led_count[strip] - self.led_starting_positions[strip]):
-            self.leds[self.led_strip[strip]][self.led_starting_positions[strip] + led] = color
+        for led in range(self.led_count[strip]):
+            self.leds[self.led_strip[strip]][self.start[strip] + led] = color
         self.leds[self.led_strip[strip]].write()
         await sleep(delay)
         if kill:
@@ -53,26 +72,33 @@ class NeopixelController:
             else:
                 raise ValueError(f"Unknown mode. '{kill_mode}'. Available modes are: {MODES}")
 
-    async def racing(self, strip: int, baseColor: "tuple[int, int, int]", racingColor: "tuple[int, int, int]", length: int, delay: float) -> None:
-        for led in range(self.led_starting_positions[strip], self.led_count[strip]):
-            self.leds[self.led_strip[strip]][led] = baseColor
+    async def chasing(
+        self,
+        strip: int,
+        base_color: "tuple[int, int, int]",
+        racing_color: "tuple[int, int, int]",
+        mix: int,
+        step_delay: float,
+        length: int,
+        frequency: int,
+    ) -> None:
+        intermediate_colors: "list[list[int]]" = [[int((1 - fade_step / mix) * rgb_1 + fade_step / mix * rgb_2) for rgb_1, rgb_2 in zip(base_color, racing_color)] for fade_step in range(mix + 1)]
+        position: int = 0
         while True:
-            for led in range(self.led_starting_positions[strip], self.led_count[strip]):
-                self.leds[self.led_strip[strip]][led] = racingColor
-                if led - length >= self.led_starting_positions[strip]:
-                    self.leds[self.led_strip[strip]][led - length] = baseColor
-                if led - length < self.led_starting_positions[strip]:
-                    self.leds[self.led_strip[strip]][self.led_count[strip] - self.led_starting_positions[strip] + led - length] = baseColor
-                self.leds[self.led_strip[strip]].write()
-                await sleep(delay)
+            for led in range(self.led_count[strip]):
+                self.leds[self.led_strip[strip]][self.start[strip] + led] = intermediate_colors[int((len(intermediate_colors) - 1) / (abs(sin(frequency * min(abs(position - led) / length, (self.led_count[strip] - abs(position - led)) / length))) + 1))]
+            position = position + 1 if position < self.led_count[strip] else 0
+            self.leds[self.led_strip[strip]].write()
+            await sleep(step_delay)
 
-async def set_mode() -> None:
+
+async def set_mode(controller: NeopixelController) -> None:
     global tasks
     global MODES
     global FUNCTIONS
     global character
 
-    uart = UART(0, 9600, parity=None, stop = 1, bits = 8, tx=Pin(0), rx=Pin(1), timeout=10)
+    uart = UART(0, 9600, parity=None, stop=1, bits=8, tx=Pin(0), rx=Pin(1), timeout=10)
     select_poll = poll()
     select_poll.register(stdin, POLLIN)
     mode_names = []
@@ -81,8 +107,11 @@ async def set_mode() -> None:
 
     while True:
         if bootsel_button() == 1:
+            for led in controller.leds:
+                led.fill((0, 0, 0))
+                led.write()
             soft_reset()
-        
+
         if uart.any() > 0:
             received_input = uart.read(1).decode("utf-8")
             if received_input != "\n":
@@ -90,13 +119,13 @@ async def set_mode() -> None:
                     character = received_input
                 else:
                     print("Unknown Character")
-        
+
         if select_poll.poll(0):
             received_input = stdin.read(1)
             if received_input != "\n":
                 if received_input in mode_names:
-                        if character != "D" and character != "E" or received_input == "A":
-                            character = received_input
+                    if character != "D" and character != "E" or received_input == "A":
+                        character = received_input
                 else:
                     print("Unknown Character")
 
@@ -107,43 +136,92 @@ async def set_mode() -> None:
                         task[1].cancel()
                     except:
                         pass
-                    tasks[count] = [character, eval(FUNCTIONS[MODES[character][count]], globals(), {"count":count})]
+                    tasks[count] = [character, eval(FUNCTIONS[MODES[character][count]], globals(), {"count": count})]
         await sleep(0.1)
 
-np = NeopixelController(pin_numbers=[2, 3, 4, 5], pin_counts=[44, 26, 12, 26], leds=[
-    [{"start":1, "count":26}, {"start":27, "count":44}],
-    [{"start":1, "count":26}],
-    [{"start":1, "count":12}],
-    [{"start":1, "count":26}]
-])
 
-tasks = []
-for _ in np.led_count:
-    tasks.append(["", None])
+controller = NeopixelController(
+    pin_numbers=(2, 3, 4, 5),
+    pin_counts=(44, 26, 12, 26),
+    leds=(
+        (
+            {"start": 1, "end": 26},
+            {"start": 27, "end": 44},
+        ),
+        (
+            {"start": 1, "end": 26},
+        ),
+        (
+            {"start": 1, "end": 12},
+        ),
+        (
+            {"start": 1, "end": 26},
+        ),
+    ),
+)
+
+tasks = [["", None] for _ in controller.end]
+
 MODES = {
-    "A": ("", "", "", "", ""),
-    "D": ("Racing", "Team Colors", "Racing", "Team Colors", "Team Colors"),
-    "E": ("Rainbow", "Rainbow", "Rainbow", "Rainbow", "Rainbow"),
-    "X": ("Racing", "Team Colors", "Racing", "Team Colors", "Team Colors"),
-    "N": ("Detected Note", "Team Colors", "Detected Note", "Team Colors", "Team Colors"),
-    "G": ("Possessed Note", "Possessed Note", "Possessed Note", "Possessed Note", "Possessed Note")
+    "A": (
+        "",
+        "",
+        "",
+        "",
+        "",
+    ),
+    "D": (
+        "Racing",
+        "Team Colors",
+        "Racing",
+        "Team Colors",
+        "Team Colors",
+    ),
+    "E": (
+        "Rainbow",
+        "Rainbow",
+        "Rainbow",
+        "Rainbow",
+        "Rainbow",
+    ),
+    "X": (
+        "Racing",
+        "Team Colors",
+        "Racing",
+        "Team Colors",
+        "Team Colors",
+    ),
+    "N": (
+        "Detected Note",
+        "Team Colors",
+        "Detected Note",
+        "Team Colors",
+        "Team Colors",
+    ),
+    "G": (
+        "Possessed Note",
+        "Possessed Note",
+        "Possessed Note",
+        "Possessed Note",
+        "Possessed Note",
+    ),
 }
 FUNCTIONS = {
-    "Team Colors": const("create_task(np.color_fade(strip=count, colors=[(0, 0, 200), (200, 0, 200)], mix=128, step_delay=0.01, delay=0.8))"),
-    "Rainbow": const("create_task(np.color_fade(strip=count, colors=[(255, 0, 0), (0, 255, 0), (0, 0, 255)], mix=128, step_delay=0.01, delay=0))"),
-    "Detected Note": const("create_task(np.static_color(strip=count, color=(255, 40, 0), delay=1, kill=False, kill_mode=''))"),
-    "Possessed Note": const("create_task(np.static_color(strip=count, color=(0, 255, 0), delay=2, kill=True, kill_mode='X'))"),
-    "Racing": const("create_task(np.racing(strip=count, baseColor=(0, 0, 200), racingColor=(200, 0, 200), length=12, delay=0.15))")
+    "Team Colors": const("create_task(controller.color_fade(strip=count, colors=[(0, 0, 200), (200, 0, 200)], mix=128, step_delay=0.01, delay=0.8))"),
+    "Rainbow": const("create_task(controller.color_fade(strip=count, colors=[(255, 0, 0), (0, 255, 0), (0, 0, 255)], mix=128, step_delay=0.01, delay=0))"),
+    "Detected Note": const("create_task(controller.static_color(strip=count, color=(255, 40, 0), delay=1, kill=False, kill_mode=''))"),
+    "Possessed Note": const("create_task(controller.static_color(strip=count, color=(0, 255, 0), delay=2, kill=True, kill_mode='X'))"),
+    "Racing": const("create_task(controller.chasing(strip=count, base_color=(0, 0, 200), racing_color=(200, 0, 200), mix=100, step_delay=0.1, length=10, frequency=1))"),
 }
 character = "D"
 
 try:
-    run(set_mode())
+    run(set_mode(controller))
 except Exception as e:
     print_exception(e)
     sleep(1)
 finally:
-    for led in np.leds:
+    for led in controller.leds:
         led.fill((0, 0, 0))
         led.write()
     soft_reset()
